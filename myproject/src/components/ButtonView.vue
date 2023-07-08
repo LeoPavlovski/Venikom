59cbfb845ef4b5b749e613ac4cca63237b853ec9
LeoPavlovski <lp29379@seeu.edu.mk>
2023-07-08 13:51:35 +0200

    first project

---
+++ myproject/src/components/ButtonView.vue
@@ -0,0 +1,26 @@
+<template>
+ <button style="background-color:black;color:white;border-radius:15px;cursor:pointer;" class="py-5"
+ @click="incrementCounter"
+ >
+ <p>Counter : {{counter}}</p>
+ </button>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      counter:0,
+    }
+  },
+  methods:{
+    incrementCounter (){
+     return this.counter++;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

36d9ba6e3ce9572db1b1e255327486d482c9c89f
LeoPavlovski <lp29379@seeu.edu.mk>
2024-01-21 19:25:58 +0100

    fix : Overlay works properly, handling the darker images in the lower screens .

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -11,21 +11,26 @@
           style="position:relative; cursor:pointer;"
       >
         <div>
+          <div
+              :class="{ 'darker-overlay': $vuetify.breakpoint.mdAndDown }"
+              :style="{ position: 'relative', width: '100%', height: $vuetify.breakpoint.smAndDown ? '200px' : '400px' }"
+          >
           <img
               :src="image.src"
               :style="{ width: '100%', height: $vuetify.breakpoint.smAndDown ? '200px' : '400px', objectFit: 'cover', overflow: 'hidden' }"
           >
-          <div class="layer">
+          <div :class="{'layer': !$vuetify.breakpoint.mdAndDown}">
             <div style="position:absolute;top:10%;left:20px; text-align: left;">
-              <h6 style="font-size:20px; color:white; font-weight:normal">
+              <h6  style="font-size:20px; font-weight:normal; color:white">
                 Explainer | Health
               </h6>
-              <h6 style="font-size:25px; color:white; margin-bottom:30px;" class="mt-5">Plant-Based Meat:<br>Healthy Option or Vegan<br>Junk Food?</h6>
-              <h6 style="font-size:20px; font-weight:normal; max-width:70%; color:white; ">Plant-based meat comes in many forms, some healthier than others.</h6>
-              <h6 style="font-size:20px; font-weight:normal; color:white;" class="mt-3">By <strong>Tracy Keeling</strong></h6>
-              <h6 style="font-size:20px; font-weight:normal; color:white;" class="mt-3">November 14, 2023 - 5 min read</h6>
+              <h6  style="font-size:25px;  margin-bottom:30px; color:white " class="mt-5">Plant-Based Meat:<br>Healthy Option or Vegan<br>Junk Food?</h6>
+              <h6  style="font-size:20px; font-weight:normal; color:white; max-width:70%; ">Plant-based meat comes in many forms, some healthier than others.</h6>
+              <h6  style="font-size:20px; font-weight:normal; color:white  " class="mt-3">By <strong>Tracy Keeling</strong></h6>
+              <h6   style="font-size:20px; font-weight:normal; color:white; " class="mt-3">November 14, 2023 - 5 min read</h6>
               <v-btn style="margin-top:50px;" outlined color="white">Read More</v-btn>
             </div>
+          </div>
           </div>
         </div>
       </v-col>
@@ -48,6 +53,21 @@ export default {
 };
 </script>
 <style>
+.darker-overlay {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.darker-overlay::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5); /* Adjust the opacity as needed */
+}
 .layer{
   background:transparent;
   height:100%;

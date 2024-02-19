343ea678b296e13becf9843658bd325a56b0b50c
LeoPavlovski <lp29379@seeu.edu.mk>
2024-02-19 22:16:17 +0100

    fix : google maps fixes.

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -177,9 +177,9 @@
           </v-row>
         </v-card>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2967.454838266546!2d20.986322877285897!3d41.94756456106028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1353fa2db6a8adb3%3A0xb8fb0d4c5ebab63d!2sVeni%20Kom!5e0!3m2!1sen!2smk!4v1705958666206!5m2!1sen!2smk" width="100%" height="450" :style="{ 'margin-top': $vuetify.breakpoint.smAndDown ? '400px' : '150px' }" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-      </iframe>
     </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2967.454838266546!2d20.986322877285897!3d41.94756456106028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1353fa2db6a8adb3%3A0xb8fb0d4c5ebab63d!2sVeni%20Kom!5e0!3m2!1sen!2smk!4v1705958666206!5m2!1sen!2smk" width="100%"  :style="{ 'margin-top': $vuetify.breakpoint.smAndDown ? '400px' : '150px' }" style="border:0; position:absolute;left:0;bottom:0; height:350px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
 
     </div>
 </template>
